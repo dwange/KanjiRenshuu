4139e28c867cfa470c54b1297f0c5178255dbf62
 Katya Savina <dwange@yandex.ru>
2025-02-13 16:48:30 +0300

    fixed the blueprint using SVGKit, removed Ungraded kanji section,

--- KanjiRenshuu/KanjiRenshuu/SelectKanjiViewController.swift
+++ KanjiRenshuu/KanjiRenshuu/SelectKanjiViewController.swift
@@ -89,9 +89,11 @@ class SelectKanjiViewController: UIViewController {
     private func fetchKanji() {
         kanjiManager.fetchAllKanji { [weak self] groupedKanji in
             guard let self = self else { return }
-            self.kanjiByGrade = groupedKanji
-            
-            self.sortedGrades = groupedKanji.keys.sorted { $0 ?? Int.max < $1 ?? Int.max }
+            let filteredGroupedKanji = groupedKanji.filter { (key, value) in
+                return key != nil
+            }
+            self.kanjiByGrade = filteredGroupedKanji
+            self.sortedGrades = filteredGroupedKanji.keys.sorted { $0 ?? Int.max < $1 ?? Int.max }
             
             DispatchQueue.main.async {
                 self.collectionView.reloadData()
